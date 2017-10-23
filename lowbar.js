eeb50cae6298ad880c0da8ad9df844f69d8dad91
jonesyk28 <jonesyk28@gmail.com>
2017-10-23 23:36:01 +0100

    implement identity

--- lowbar.js
+++ lowbar.js
@@ -6,6 +6,17 @@ const _ = {};
 _.identity = arg => arg;
 
 
+//////////////////////values///////////////////////////////////////////
+
+_.values = (list) => {
+  if (typeof (list) !== 'object') return [];
+  if (Array.isArray(list)) return list;
+  let newArray = [];
+  for (let key in list) newArray.push(list[key]);
+  return newArray;
+};
+   
+
 
 
 
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -27,6 +27,26 @@ describe('_', function () {
   });
 
 
+  ///////////////////////////test values//////////////////////////////////////////////
+
+  describe('#values', function () {
+    it('is a function', function () {
+      expect(_.values).to.be.a('function');
+    });
+    it('Return all of the values of the objects own properties.', () => {
+      expect(_.values({ a: 1, b: 2, c: 3 })).to.eql([1, 2, 3]);
+      expect(_.values({ 1: 'a', 2: 'b' })).to.eql(['a', 'b']);
+    });
+    it('Return empty array if arguments are not an object.', () => {
+      expect(_.values('string')).to.eql([]);
+      expect(_.values(false)).to.eql([]);
+      expect(_.values(1)).to.eql([]);
+    });
+    it('Return array if passed an array.', () => {
+      expect(_.values([1, 2, 3])).to.eql([1, 2, 3]);
+    });
+  });
+
 
 
 });
